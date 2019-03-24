abb867a7d730213a2801b0d0de6c131d68459284
syzer3@gmail.com <syzer3@gmail.com>
2019-03-24 09:47:59 +0100

    Add images to first promotion

--- discounts_server/manual_product.py
+++ discounts_server/manual_product.py
@@ -3,27 +3,31 @@ import json
 
 
 def moroccan_tea():
-
     tea_dict = {
-      'id':5335294524,
-      'name': 'Sirocco Moroccan Mint Tee',
-      'accessible_information_text': 'Im Gegensatz zu der in Deutschland ' \
-                                     ' und der Schweiz weit verbreiteten Pfefferminze stammt die Minze für den Moroccan' \
-                                     ' Mint von Sirocca aus Marokko. Die so genannte Nanaminze ist eine natürliche' \
-                                     ' Varietät der Grünen Minze (engl. "Spearmint") und wesentlich milder und ' \
-                                     'süsslicher im Geschmack. Die Minze enthält kein Menthol und ist daher auch für ' \
-                                     'Geniesser mit empfindlichem Magen zu empfehlen. Die Minze wirkt sehr ' \
-                                     'durstlöschend und das lang anhaltende Aroma ist ein wahrer Genuss. ' \
-                                     'Erst ganz wenige Mengen reiner marokkanischer Minze werden überhaupt ' \
-                                     'exportiert – und entsprechend gering fallen die Mengen an verfügbarer ' \
-                                     'biologischer Minze aus. Um die verfügbare Menge zu erhöhen, baut Sirocco ' \
-                                     'mittlerweile selbst marokkanische Minze in nachhaltiger Qualität an.',
-      'type': 'product',
-      'price': 16.0,
-      'original_price': 17.2}
+        'id': 5335294524,
+        'name': 'Sirocco Moroccan Mint Tee',
+        'accessible_information_text': 'Im Gegensatz zu der in Deutschland ' \
+                                       ' und der Schweiz weit verbreiteten Pfefferminze stammt die Minze für den Moroccan' \
+                                       ' Mint von Sirocca aus Marokko. Die so genannte Nanaminze ist eine natürliche' \
+                                       ' Varietät der Grünen Minze (engl. "Spearmint") und wesentlich milder und ' \
+                                       'süsslicher im Geschmack. Die Minze enthält kein Menthol und ist daher auch für ' \
+                                       'Geniesser mit empfindlichem Magen zu empfehlen. Die Minze wirkt sehr ' \
+                                       'durstlöschend und das lang anhaltende Aroma ist ein wahrer Genuss. ' \
+                                       'Erst ganz wenige Mengen reiner marokkanischer Minze werden überhaupt ' \
+                                       'exportiert – und entsprechend gering fallen die Mengen an verfügbarer ' \
+                                       'biologischer Minze aus. Um die verfügbare Menge zu erhöhen, baut Sirocco ' \
+                                       'mittlerweile selbst marokkanische Minze in nachhaltiger Qualität an.',
+        'type': 'product',
+        'price': 16.0,
+        'original_price': 17.2,
+        'image': {
+            'src': "siroco.jpg",
+            'retina_src': "siroco.jpg",
+            'original': "siroco.jpg"
+        },
+    }
 
     return tea_dict
-
 
 
 print(moroccan_tea())
